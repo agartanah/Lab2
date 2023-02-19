003428c7ceac7709ee72d636325a78c0780b5b24
agartanah <vyatin2017@mail.ru>
2023-02-19 14:20:32 +0300

    Create class TXT

--- TXT.cs
+++ TXT.cs
@@ -5,6 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace lab2 {
-  class TXT {
+  class TXT : Document {
+    int DocumentTXTStringCount, DocumentTXTSignCount;
+
+    public TXT(string DocumentTitle, string DocumentAuthor, string DocumentKeyWord, string DocumentPath,
+      string DocumentTopic) {
+      this.DocumentTitle = DocumentTitle;
+      this.DocumentAuthor = DocumentAuthor;
+      this.DocumentKeyWord = DocumentKeyWord;
+      this.DocumentPath = DocumentPath;
+      this.DocumentTopic = DocumentTopic;
+
+      Console.WriteLine("Введите количество символов: ");
+      DocumentTXTSignCount = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("Введите количество строк: ");
+      DocumentTXTStringCount = int.Parse(Console.ReadLine());
+    }
+
+    public override void DocumentInfo() {
+      Console.WriteLine($"\nИмя: {DocumentTitle}\nАвтор: {DocumentAuthor}\nКлючевые слова: {DocumentKeyWord}\n" +
+        $"Путь к файлу: {DocumentPath}\nТема документа: {DocumentTopic}\nКоличество строк: {DocumentTXTStringCount}\n" +
+        $"Количество символов: {DocumentTXTSignCount}\n");
+    }
   }
 }
