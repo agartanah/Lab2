db4a12715d28b4dd179c0b5397b01e6cd0f5e769
agartanah <vyatin2017@mail.ru>
2023-02-19 14:04:51 +0300

    Create parental class Document

---
+++ Document.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2 {
+  class Document {
+    public string DocumentTitle, DocumentAuthor,
+      DocumentKeyWord, DocumentPath, DocumentTopic;
+
+    public virtual void DocumentInfo() {
+      Console.WriteLine($"Title: {DocumentTitle}\nAuthor: {DocumentAuthor}\nKey word: {DocumentKeyWord}\n" +
+        $"Path: {DocumentPath}\nTopic: {DocumentTopic}\n");
+    }
+  }
+}
