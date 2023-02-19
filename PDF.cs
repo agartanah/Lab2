8e7a7b8c39fb12c15b444ab78bc79294e5383b51
agartanah <vyatin2017@mail.ru>
2023-02-19 14:13:15 +0300

    Create class PDF

---
+++ PDF.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2 {
+  class PDF : Document {
+    string DocumentPDFDateEdit, DocumentPDFPageCount;
+
+    public PDF(string DocumentTitle, string DocumentAuthor, string DocumentKeyWord, string DocumentPath,
+      string DocumentTopic) {
+      this.DocumentTitle = DocumentTitle;
+      this.DocumentAuthor = DocumentAuthor;
+      this.DocumentKeyWord = DocumentKeyWord;
+      this.DocumentPath = DocumentPath;
+      this.DocumentTopic = DocumentTopic;
+
+      Console.WriteLine("Введите количество строк: ");
+      DocumentPDFPageCount = Console.ReadLine();
+
+      Console.WriteLine("Введите используемые классы: ");
+      DocumentPDFDateEdit = Console.ReadLine();
+    }
+
+    public override void DocumentInfo() {
+      Console.WriteLine($"\nИмя: {DocumentTitle}\nАвтор: {DocumentAuthor}\nКлючевые слова: {DocumentKeyWord}\n" +
+        $"Путь к файлу: {DocumentPath}\nТема документа: {DocumentTopic}\nДата редактирования: {DocumentPDFDateEdit}\n" +
+        $"Количество страниц: {DocumentPDFPageCount}\n");
+    }
+  }
+}
